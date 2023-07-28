e06a2789ef720cf7be1c52dd8c77f10a50a18e8a
Jax121243 <101913901+Jax121243@users.noreply.github.com>
2023-07-28 21:41:59 +0800

    Update evil_portal_scene_console_output.c

--- flipper/flipper-evil-portal/scenes/evil_portal_scene_console_output.c
+++ flipper/flipper-evil-portal/scenes/evil_portal_scene_console_output.c
@@ -88,7 +88,6 @@ void evil_portal_scene_console_output_on_enter(void *context) {
     }
 
     if (0 == strncmp(LED_CMD, app->selected_tx_string, strlen(LED_CMD))) {
-      app->sent_led = true;
       if (app->show_stopscan_tip) {
         const char *msg = "LED toggled :)\nPress BACK to return\n";
         furi_string_cat_str(app->text_box_store, msg);
